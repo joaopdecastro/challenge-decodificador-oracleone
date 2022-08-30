1a92074e318153ebe254550b8d1a33fecd9f28d5
joaopdecastro <joaopaulocastro.s@gmail.com>
2022-08-30 16:18:43 -0300

    Ajustes no HTML e criação do motor de criptografia

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Funções que atualizam a tela de mensagem
 function telaSemTexto () {
     let output = document.getElementById('output')
     let botaoCopiar = document.getElementById('button-copiar')
@@ -11,15 +10,55 @@ function telaSemTexto () {
 
 function telaComTexto() {
     let semTexto = document.getElementById('sem-texto')
+    let output = document.getElementById('output')
+    let botaoCopiar = document.getElementById('button-copiar')
 
     semTexto.style.display = 'none'
+    output.style.display = 'block'
+    botaoCopiar.style.display = 'block'
 }
 
+function criptografar () {
 
-var textoCriptografado = document.getElementById('texto-criptografado')
+    let resultado = ''
 
-if (textoCriptografado.value == '') {
-    telaSemTexto()
-} else {
+    for (let i = 0; i < input.value.length; i++) {
+
+        if (input.value[i] == 'e') {
+            resultado = resultado + 'enter'
+        } else if (input.value[i] == 'i') {
+            resultado = resultado + 'imes'
+        } else if (input.value[i] == 'a') {
+            resultado = resultado + 'ai'
+        } else if (input.value[i] == 'o') {
+            resultado = resultado + 'ober'
+        } else if (input.value[i] == 'u') {
+            resultado = resultado + 'ufat'
+        }else {
+            resultado = resultado + input.value[i]
+        }
+        
+    }
+   
+    output.innerText = resultado
+
     telaComTexto()
 }
+
+function descriptografar () {
+
+//criar função
+
+}
+
+var input = document.getElementById('input-text')
+var output = document.getElementById('texto-criptografado')
+
+telaSemTexto()
+
+var buttonCripto = document.getElementById('button-criptografar')
+buttonCripto.onclick = criptografar
+
+
+
+
